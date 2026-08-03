ad7fe5f6c805b31c02535ab029018198090d2fa9
namma-ai-sa <abdulrahman.s.alshamsan@gmail.com>
2026-08-03 16:04:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function generateContent(){
-alert("🚀 نمّى AI يعمل");
+
+document.getElementById("articleResult").innerText =
+"كيف تبدأ مشروعك باستخدام الذكاء الاصطناعي";
+
+document.getElementById("seoResult").innerText =
+"دليل شامل لاستخدام الذكاء الاصطناعي في تطوير الأعمال وزيادة الإنتاجية.";
+
+document.getElementById("socialResult").innerText =
+"الذكاء الاصطناعي لم يعد رفاهية بل أداة أساسية لتسريع النمو.";
+
+document.getElementById("videoResult").innerText =
+"5 طرق يغير بها الذكاء الاصطناعي مستقبل الأعمال.";
+
+document.getElementById("imageResult").innerText =
+"رائد أعمال يعمل على حاسوب حديث مع عناصر ذكاء اصطناعي مستقبلية.";
+
 }
