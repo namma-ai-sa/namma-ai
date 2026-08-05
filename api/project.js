f2ec9789d52edf9dd5a4e40db812e54d252cf874
namma-ai-sa <abdulrahman.s.alshamsan@gmail.com>
2026-08-05 07:37:52 +0300

    Update project.js

--- api/project.js
+++ api/project.js
@@ -1,4 +1,9 @@
-import supabase from "./supabase.js";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 
 export default async function handler(req, res) {
 
